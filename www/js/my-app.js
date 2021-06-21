083fc8db9acd92f6a3ca248d989ca8130afd819e
ElenaSH91 <elena.sanchez.hermosilla@gmail.com>
2021-06-20 21:05:46 -0300

    my-app.js: V0.0.1

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -19,8 +19,6 @@ var app = new Framework7({
       {path: '/ingresos/', url: 'ingresos.html',},
       {path: '/gastos/', url: 'gastos.html',},
       {path: '/cuentas/', url: 'cuentas.html',},
-      {path: '/ahorros/', url: 'ahorros.html',},
-      {path: '/listaCompras/', url: 'listaCompras.html',},
       {path: '/informes/', url: 'informes.html',},
       {path: '/config/', url: 'config.html',},
       {path: '/mas/', url: 'mas.html',},
@@ -39,59 +37,88 @@ $$(document).on('deviceready', function() {
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
     console.log(e);
+    document.getElementById('irAtras').style.visibility='hidden'; //Esconde la flecha "back"
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //
+document.getElementById('irAtras').style.visibility='hidden'; //Esconde la flecha "back"
+$$("#irAIngresos").on("click",fnIrAIngresos); //Redirecciona a la página Ingresos
+$$("#irAGastos").on("click",fnIrAGastos); //Redirecciona a la página Gastos
+$$("#irACuentas").on("click",fnIrACuentas); //Redirecciona a la página Cuentas
+$$("#irAInformes").on("click",fnIrAInformes); //Redirecciona a la página Informes
+$$("#irAMas").on("click",fnIrAMas); //Redirecciona a la página Más
+$$("#irAConfig").on("click",fnIrAConfig); //Redirecciona a la página Configuraciones
 })
 
 $$(document).on('page:init', '.page[data-name="ingresos"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //
+$$("#irAtras").on("click",fnIrAtras);
+document.getElementById('irAtras').style.visibility='visible'; //Muestra la flecha "back"
 })
 
 $$(document).on('page:init', '.page[data-name="gastos"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //
+$$("#irAtras").on("click",fnIrAtras);
+document.getElementById('irAtras').style.visibility='visible'; //Muestra la flecha "back"
 })
 
 $$(document).on('page:init', '.page[data-name="cuentas"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
-})
-
-$$(document).on('page:init', '.page[data-name="ahorros"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
-})
-
-$$(document).on('page:init', '.page[data-name="listaCompras"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //
+$$("#irAtras").on("click",fnIrAtras);
+document.getElementById('irAtras').style.visibility='visible'; //Muestra la flecha "back"
 })
 
 $$(document).on('page:init', '.page[data-name="informes"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
-})
-
-$$(document).on('page:init', '.page[data-name="config"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //
+$$("#irAtras").on("click",fnIrAtras);
+document.getElementById('irAtras').style.visibility='visible'; //Muestra la flecha "back"
 })
 
 $$(document).on('page:init', '.page[data-name="mas"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    //
+$$("#irAtras").on("click",fnIrAtras);
+document.getElementById('irAtras').style.visibility='visible'; //Muestra la flecha "back"
 })
+
+$$(document).on('page:init', '.page[data-name="config"]', function (e) {
+    //
+$$("#irAtras").on("click",fnIrAtras);
+document.getElementById('irAtras').style.visibility='visible'; //Muestra la flecha "back"
+})
+
+
+
+/*Variables Globales*/
+
+
+/**Funciones**/
+function fnIrAIngresos(){
+  $$("#title_top").html("Ingresos");
+  mainView.router.navigate("/ingresos/", {transition:"f7-fade"});
+}
+function fnIrAGastos(){
+  $$("#title_top").html("Gastos");
+  mainView.router.navigate("/gastos/", {transition:"f7-fade"});
+}
+function fnIrACuentas(){
+  $$("#title_top").html("Cuentas");
+  mainView.router.navigate("/cuentas/", {transition:"f7-fade"});
+}
+function fnIrAInformes(){
+  $$("#title_top").html("Informes");
+  mainView.router.navigate("/informes/", {transition:"f7-fade"});
+}
+function fnIrAMas(){
+  $$("#title_top").html("Más");
+  mainView.router.navigate("/mas/", {transition:"f7-fade"});
+}
+function fnIrAConfig(){
+  $$("#title_top").html("Configuraciones");
+  mainView.router.navigate("/config/", {transition:"f7-fade"});
+}
+function fnIrAtras(){
+  $$("#title_top").html("Cash Admin");
+  document.getElementById('irAtras').style.visibility='hidden'; //Esconde la flecha "back"
+}
